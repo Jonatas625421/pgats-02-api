1ec72f8082764d9dd5c5911a7959f71ee8185fca
jonatas625421 <jonatas.fonseca26@hotmail.com>
2025-08-28 18:34:16 -0300

    Colocando um bug de proposito

--- service/transferService.js
+++ service/transferService.js
@@ -8,7 +8,7 @@ function transfer({ from, to, amount }) {
   const sender = users.find(u => u.username === from);
   const recipient = users.find(u => u.username === to);
 
-  if (!sender || !recipient) throw new Error('User not found.');
+  if (!sender || !recipient) throw new Error('User not, TTTTTTT found.');
   if (sender.balance < amount) throw new Error('Insufficient funds.');
   if (!recipient.favorecido && amount >= 5000) {
     throw new Error('Transfer limit for non-favorecido is R$ 5.000,00.');
